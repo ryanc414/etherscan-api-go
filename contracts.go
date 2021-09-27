a778be4f0fe1608bf10ef090a9ca8e373cb4b11f
Ryan Collingham <ryan@tokencard.io>
2021-09-27 14:15:27 +0100

    use unmarshalResponse in contracts module

--- contracts.go
+++ contracts.go
@@ -34,51 +34,19 @@ func (c ContractsClient) GetContractABI(
 }
 
 type ContractInfo struct {
-	SourceCode           string
-	ABI                  string
-	ContractName         string
-	CompilerVersion      string
-	OptimizationUsed     string
-	Runs                 uint32
-	ConstructorArguments string
-	EVMVersion           string
-	Library              string
-	LicenseType          string
-	Proxy                bool
-	Implementation       string
-	SwarmSource          string
-}
-
-type contractResult struct {
-	SourceCode       string  `json:"SourceCode"`
-	ABI              string  `json:"ABI"`
-	CompilerVersion  string  `json:"CompilerVersion"`
-	OptimizationUsed string  `json:"OptimizaitonUsed"`
-	Runs             uintStr `json:"Runs"`
-	ConstructorArgs  string  `json:"ConstructorArguments"`
-	EVMVersion       string  `json:"EVMVersion"`
-	Library          string  `json:"Library"`
-	LicenseType      string  `json:"LicenseType"`
-	Proxy            string  `json:"Proxy"`
-	Implementation   string  `json:"Implementation"`
-	SwarmSource      string  `json:"SwarmSource"`
-}
-
-func (c *contractResult) toInfo() *ContractInfo {
-	return &ContractInfo{
-		SourceCode:           c.SourceCode,
-		ABI:                  c.ABI,
-		CompilerVersion:      c.CompilerVersion,
-		OptimizationUsed:     c.OptimizationUsed,
-		Runs:                 uint32(c.Runs.unwrap()),
-		ConstructorArguments: c.ConstructorArgs,
-		EVMVersion:           c.EVMVersion,
-		Library:              c.Library,
-		LicenseType:          c.LicenseType,
-		Proxy:                c.Proxy != "0",
-		Implementation:       c.Implementation,
-		SwarmSource:          c.SwarmSource,
-	}
+	SourceCode           string `etherscan:"SourceCode"`
+	ABI                  string `etherscan:"ABI"`
+	ContractName         string `etherscan:"ContractName"`
+	CompilerVersion      string `etherscan:"CompilerVersion"`
+	OptimizationUsed     string `etherscan:"OptimizationUsed"`
+	Runs                 uint32 `etherscan:"Runs"`
+	ConstructorArguments string `etherscan:"ConstructorArguments"`
+	EVMVersion           string `etherscan:"EVMVersion"`
+	Library              string `etherscan:"Library"`
+	LicenseType          string `etherscan:"LicenseType"`
+	Proxy                bool   `etherscan:"Proxy"`
+	Implementation       string `etherscan:"Implementation"`
+	SwarmSource          string `etherscan:"SwarmSource"`
 }
 
 func (c ContractsClient) GetContractSourceCode(
@@ -93,15 +61,10 @@ func (c ContractsClient) GetContractSourceCode(
 		return nil, err
 	}
 
-	var result []contractResult
-	if err := json.Unmarshal(rspData, &result); err != nil {
+	var result []ContractInfo
+	if err := unmarshalResponse(rspData, &result); err != nil {
 		return nil, err
 	}
 
-	infos := make([]ContractInfo, len(result))
-	for i := range result {
-		infos[i] = *result[i].toInfo()
-	}
-
-	return infos, nil
+	return result, nil
 }
