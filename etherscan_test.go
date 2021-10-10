ee8c7a66f53727892490ecccc3dc36ba935d0291
Ryan Collingham <ryan@tokencard.io>
2021-10-10 22:05:25 +0100

    store query and response data for account module

--- etherscan_test.go
+++ etherscan_test.go
@@ -1,14 +1,125 @@
 package etherscan_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
+	"path"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/ryanc414/purehttp"
 )
 
 func TestMain(m *testing.M) {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	os.Exit(m.Run())
+}
+
+type mockServer struct {
+	apiKey      string
+	module      string
+	testDir     string
+	responseDir string
+	srv         *httptest.Server
+}
+
+func newMockServer(module string) mockServer {
+	m := mockServer{
+		apiKey:  uuid.NewString(),
+		module:  module,
+		testDir: path.Join("testData", module),
+	}
+
+	h := purehttp.NewHandler(m.handleRequest)
+	m.srv = httptest.NewServer(h)
+
+	return m
+}
+
+func (m *mockServer) close() {
+	m.srv.Close()
+}
+
+func (m *mockServer) handleRequest(req *http.Request) (*purehttp.Response, error) {
+	if req.URL.Path != "/api" {
+		return &purehttp.Response{
+			Body:       []byte("path not found\n"),
+			StatusCode: http.StatusNotFound,
+		}, nil
+	}
+
+	q := req.URL.Query()
+	if q.Get("module") != "account" {
+		return &purehttp.Response{
+			Body:       []byte("unknown model\n"),
+			StatusCode: http.StatusNotFound,
+		}, nil
+	}
+
+	if q.Get("apikey") != m.apiKey {
+		return &purehttp.Response{
+			Body:       []byte("unknown API key"),
+			StatusCode: http.StatusForbidden,
+		}, nil
+	}
+
+	return m.handleAction(q)
+}
+
+func (m *mockServer) handleAction(q url.Values) (*purehttp.Response, error) {
+	action := q.Get("action")
+	if action == "" {
+		return &purehttp.Response{
+			Body:       []byte("action not specified"),
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	params := filterQuery(q)
+
+	responsePath := path.Join(m.testDir, fmt.Sprintf("%s.json", action))
+	data, err := ioutil.ReadFile(responsePath)
+	if err != nil {
+		return nil, err
+	}
+
+	responses := make(map[string]json.RawMessage)
+	if err := json.Unmarshal(data, &responses); err != nil {
+		return nil, err
+	}
+
+	encoded := params.Encode()
+	rspData, ok := responses[encoded]
+	if !ok {
+		return &purehttp.Response{
+			Body:       []byte(fmt.Sprintf("query path not found: %s", encoded)),
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	return &purehttp.Response{
+		Body:       rspData,
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func filterQuery(q url.Values) url.Values {
+	params := make(url.Values, len(q))
+
+	for k, v := range q {
+		if k == "apikey" || k == "module" || k == "action" {
+			continue
+		}
+
+		params[k] = v
+	}
+
+	return params
 }
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.23.0
+	github.com/ryanc414/purehttp v0.0.0-20211002205326-91334890ff5c // indirect
 	github.com/stretchr/testify v1.7.0
 )
